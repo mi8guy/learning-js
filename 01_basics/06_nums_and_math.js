cca390b235545e5100e2cd454f31893b3ac995a4
mi8guy <155919273+mi8guy@users.noreply.github.com>
2024-01-10 09:55:50 +0000

    Num and Math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed(1)); // 100.0
+
+const otherNumber = 123.8953
+// console.log(otherNumber.toPrecision(3)); //124
+// console.log(otherNumber.toPrecision(4)); //123.9
+// console.log(otherNumber.toPrecision(2)); // 1.2e+2
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
+
+// Number.MIN_VALUE , Number.MAX_VALUE
+
+//******************************* Math *******************************/
+
+// console.log(Math.random()) // between 0 and 1
+// console.log(Math.random() * 10) // between 0 and 10
+// console.log(Math.floor(Math.random() * 10) + 1) // above 1, avoid 0
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min)) // between min and max
+
+
+
+
