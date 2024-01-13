f0839fa72610358db13285a85fe3990a1b4e8894
mi8guy <155919273+mi8guy@users.noreply.github.com>
2024-01-13 12:24:42 +0000

    null coalescing and ternary operator

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -31,5 +31,27 @@ if(Object.keys(emptyObject).length == 0){ // Object.keys(..) returns an array of
     // console.log(`Object is empty`);
 }
 
+// Nullish Coalescing operator (??): null undefined
+
+// Sometimes database calls don't give a direct response, they give two values, chances are null or undefined. These cases can disrupt code.
+// This operator is made specially for such cases.
+// if it is null, we assign null and treat code accordingly
+// if it is undefined, we assign undefined and code accordingly
+
+let val1;
+// val1 = 5 ?? 10               // 5
+// val1 = null ?? 10            // 10
+// val1 = undefined ?? 15       // 15
+// val1 = null ?? 10 ?? 20      // 10
+
+// console.log(val1);
 
 
+/** Teranary operators 
+ * 
+ *  condition ? true : false
+ * 
+*/
+
+const iceTeaPrice = 100
+// iceTeaPrice >= 80 ? console.log(`more than 80`) : console.log(`less than 80`);;
