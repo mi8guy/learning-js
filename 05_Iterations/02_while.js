480d197b2f8c26d6abbddceb349f908713e763fd
mi8guy <155919273+mi8guy@users.noreply.github.com>
2024-01-13 18:03:14 +0000

    while and do-while

---
+++ 05_Iterations/02_while.js
@@ -0,0 +1,31 @@
+// While loop 
+
+// Initialization is outside
+let index = 1
+while (index <= 10) { // condition
+    // console.log(`Value of index is ${index}`);
+    index = index + 2 // Incrementation 
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+while(arr < myArray.length){
+    // console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1 
+}
+
+// Do while loop 
+// runs once no matter what.
+
+let score = 11   // Init
+
+do{
+    console.log(`Score is ${score}`);
+    score++;        // Increment
+}while(score <= 10) // Condition
+
+
+
+
